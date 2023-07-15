87dbba28357064d479a44f844196829a20ad11b2
MulweliEdzimura <ipfitshamano82@gmail.com>
2023-07-15 10:46:25 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - Print a quote
+ * Return: 1
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",
-sizeof("and that piece of art is useful\"- Dora Korpar, 2015-10-19"));
+write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",59);
 return (1);
 }
