c38440169c5d3c70ae86bfbb118c9ee1a0f32abf
MulweliEdzimura <ipfitshamano82@gmail.com>
2023-07-22 03:02:10 -0700

    update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,12 +2,12 @@
 /**
 *print_numbers - print the numbers, from 0 to 9, followed by a new line
 * 
-*Return: void
+*Return: Always 0
 */
 void print_numbers(void)
 {
-char c;
-for (c = '0'; c <= '9'; c++)
+int c;
+for (c = 48; c < 58; c++)
 {
 _putchar(c);
 }
